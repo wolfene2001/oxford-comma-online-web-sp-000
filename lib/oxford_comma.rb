0fcdd603d342ffbc85173e80c44143de0df1c9fe
wolfene2001 <wolfene2001@yahoo.com>
2019-10-14 14:26:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
   return array.join(" and ")
 elsif array.length >= 3
-  array[-1] == "and #{array[-1]}"
+  array[-1] = "and #{array[-1]}"
   return array.join(", ")
   end
 end
