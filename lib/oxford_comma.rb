5d16021896dc744928c5ed90901b32a7717c04f4
wolfene2001 <wolfene2001@yahoo.com>
2019-10-14 14:23:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,10 @@
 def oxford_comma(array)
-  if array == [kiwi]
-    array.join(" ")
-  elsif array == [ , ]
-    array.join(" ", << and)
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+  return array.join(" and ")
+elsif array.length >= 3
+  array[-1] == "and #{array[-1]}"
+  return array.join(", ")
   end
 end
